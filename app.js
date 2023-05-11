1226d1a05e35da6b994d8965b6890ca4c0e01a96
ShantanuWare <shantanuware0763@gmail.com>
2023-05-11 14:48:13 +0530

    server

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 require('dotenv').config()
-const mongoAtlas=process.env.mongoAtlas;
+const mongoAtlas=process.env.MONGO_ATLAS;
 
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
